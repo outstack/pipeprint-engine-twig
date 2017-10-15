83b588315d3e25b7b513abc76dd457171c93102b
Adam Quaile <adamquaile@gmail.com>
2017-10-15 11:58:18 +0100

    Changes after first runthrough

--- web/app.php
+++ web/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -13,11 +14,24 @@ $twig = new Twig_Environment($loader, array(
 
 $app->post('/render', function (Request $request) use ($app, $twig) {
     $data = json_decode($request->getContent(), true);
+    if (JSON_ERROR_NONE !== json_last_error()) {
+        return new JsonResponse(
+            [
+                'error' => 'json_parse_error',
+                'description' => json_last_error_msg()
+            ],
+            400
+        );
+    }
 
     $template = $data['files'][$data['template']];
 
     return new Response(
-        $twig->createTemplate($template)->render($data['parameters'])
+        $twig->createTemplate($template)->render($data['parameters']),
+        200,
+        [
+            'Content-type' => 'text/plain'
+        ]
     );
 });
 
